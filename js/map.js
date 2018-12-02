8fe456196070826cca36841f30205c11ad026ffd
Littletheodore <balandinamo@gmail.com>
2018-12-02 15:33:45 +0300

    Внесены последние изменения

--- js/map.js
+++ js/map.js
@@ -11,6 +11,8 @@ var randomArray = function (variants) {
   var newArray = [];
   for (var i = 0; i < randomNumber(variants.length, 0); i++) {
     newArray[i] = variants[randomNumber((variants.length - 1), 0)];
+
+
     for (var j = 0; j < i; j++) {
       if (newArray[i] === newArray[j]) {
         newArray[i] = '';
@@ -49,14 +51,13 @@ for (i = 0; i < 8; i++) {
   };
   hosts[i] = host;
 }
-
+console.log(hosts);
 var map = document.querySelector('.map');
-map.classList.remove('map--faded');
 
+//Отрисовка меток на карте
 var mapPins = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content.querySelector('button');
 var fragment = document.createDocumentFragment();
-
 for (var i = 0; i < 8; i++) {
   var element = pinTemplate.cloneNode(true);
   element.style = 'left:' + (hosts[i].location.x + 25) + 'px; ' + 'top:' + (hosts[i].location.y + 70) + 'px;';
@@ -64,53 +65,113 @@ for (var i = 0; i < 8; i++) {
   element.children[0].alt = hosts[i].offer.title;
   fragment.appendChild(element);
 }
-mapPins.appendChild(fragment);
 
-var cardTemplate = document.querySelector('#card').content.querySelector('article');
-var mainCard = cardTemplate.cloneNode(true);
+//Заполнение карточки выбранной метки
 
-mainCard.children[0].src = hosts[0].author.avatar;
-mainCard.children[2].textContent = hosts[0].offer.title;
-mainCard.children[3].textContent = hosts[0].offer.adress;
-mainCard.children[4].innerHTML = hosts[0].offer.price + '&#x20bd;<span>/ночь</span>';
+var fillMainCard = function (pinNumber) {
 
-if (hosts[0].offer.type === 'flat') {
-  mainCard.children[5].textContent = 'Квартира';
-} else if (hosts[0].offer.type === 'bungalo') {
-  mainCard.children[5].textContent = 'Бунгало';
-} else if (hosts[0].offer.type === 'house') {
-  mainCard.children[5].textContent = 'Дом';
-} else mainCard.children[5].textContent = 'Дворец';
+  var oldPopup = document.querySelector('article.popup');
+  if (oldPopup) {
+    map.removeChild(oldPopup);
+  }
 
-mainCard.children[6].textContent = hosts[0].offer.rooms + ' комнаты для ' + hosts[0].offer.guests + ' гостей';
-mainCard.children[7].textContent = 'Заезд после ' + hosts[0].offer.checkin + ', выезд до ' + hosts[0].offer.checkout;
+  var cardTemplate = document.querySelector('#card').content.querySelector('article');
+  var mainCard = cardTemplate.cloneNode(true);
 
-var featuresList = mainCard.children[8];
-var newFeaturesArray = [];
+  mainCard.children[0].src = hosts[pinNumber].author.avatar;
+  mainCard.children[2].textContent = hosts[pinNumber].offer.title;
+  mainCard.children[3].textContent = hosts[pinNumber].offer.adress;
+  mainCard.children[4].innerHTML = hosts[pinNumber].offer.price + '&#x20bd;<span>/ночь</span>';
 
-for (var i = 0; i < featuresVariants.length; i++) {
-  newFeaturesArray[i] = featuresList.children[0];
-  featuresList.removeChild(featuresList.children[0]);
-}
+  if (hosts[pinNumber].offer.type === 'flat') {
+    mainCard.children[5].textContent = 'Квартира';
+  } else if (hosts[pinNumber].offer.type === 'bungalo') {
+    mainCard.children[5].textContent = 'Бунгало';
+  } else if (hosts[pinNumber].offer.type === 'house') {
+    mainCard.children[5].textContent = 'Дом';
+  } else mainCard.children[5].textContent = 'Дворец';
 
-for (var i = 0; i < (hosts[0].offer.features.length); i++) {
-  var feature = hosts[0].offer.features[i];
-  for (var j = 0; j < newFeaturesArray.length - 1; j++) {
-    var icon = newFeaturesArray[j].classList;
-    if (icon[1] === 'popup__feature--' + feature) {
-      featuresList.appendChild(newFeaturesArray[j]);
+  mainCard.children[6].textContent = hosts[pinNumber].offer.rooms + ' комнаты для ' + hosts[pinNumber].offer.guests + ' гостей';
+  mainCard.children[7].textContent = 'Заезд после ' + hosts[pinNumber].offer.checkin + ', выезд до ' + hosts[pinNumber].offer.checkout;
+
+  var featuresList = mainCard.children[8];
+  var newFeaturesArray = [];
+  for (var i = 0; i < featuresVariants.length; i++) {
+    newFeaturesArray[i] = featuresList.children[0];
+    featuresList.removeChild(featuresList.children[0]);
+  }
+  for (var i = 0; i < (hosts[pinNumber].offer.features.length); i++) {
+    var feature = hosts[pinNumber].offer.features[i];
+    for (var j = 0; j < newFeaturesArray.length; j++) {
+      var icon = newFeaturesArray[j].classList;
+      if (icon[1] === 'popup__feature--' + feature) {
+        featuresList.appendChild(newFeaturesArray[j]);
+      }
     }
   }
+
+  mainCard.children[9].textContent = hosts[pinNumber].offer.description;
+
+  var popupPhotos = mainCard.children[10];
+  popupPhotos.children[0].src = hosts[pinNumber].offer.photos[0];
+  for (var i = 1; i < (hosts[pinNumber].offer.photos.length); i++) {
+    var newPhoto = popupPhotos.children[0].cloneNode(true);
+    newPhoto.src = hosts[pinNumber].offer.photos[i];
+    popupPhotos.appendChild(newPhoto);
+  }
+  map.insertBefore(mainCard, map.children[1]);
+};
+
+//Переход в неактивное состояние форм
+var noticeFieldsets = document.querySelectorAll('.ad-form>fieldset');
+for (var i = 0; i < noticeFieldsets.length; i++) {
+  noticeFieldsets[i].disabled = 'disabled';
 }
+var mapFiltersForm = document.querySelector('.map__filters');
+mapFiltersForm.classList.add('map__filters--disabled');
+var noticeForm = document.querySelector('.ad-form');
 
-mainCard.children[9].textContent = hosts[0].offer.description;
 
-var popupPhotos = mainCard.children[10];
-popupPhotos.children[0].src = hosts[0].offer.photos[0];
-for (var i = 1; i < (hosts[0].offer.photos.length); i++) {
-  var newPhoto = popupPhotos.children[0].cloneNode(true);
-  newPhoto.src = hosts[0].offer.photos[i];
-  popupPhotos.appendChild(newPhoto);
-}
 
-map.insertBefore(mainCard, map.children[1]);
+var activeModeOn = function () {
+  map.classList.remove('map--faded');
+  mapPins.appendChild(fragment);
+  mapFiltersForm.classList.remove('map__filters--disabled');
+  noticeForm.classList.remove('ad-form--disabled');
+  for (var i = 0; i < noticeFieldsets.length; i++) {
+    noticeFieldsets[i].disabled = '';
+  }
+};
+
+//Заполнение поля адрес
+var mainIconSize = 65;
+var fillAdress = function (iconX, iconY) {
+  var adressInput = document.querySelector('#address');
+  adressInput.setAttribute('value', Math.round(iconX + mainIconSize / 2) + ',' + Math.round(iconY + mainIconSize / 2));
+  adressInput.setAttribute('readonly', 'readonly');
+};
+
+//Переход в активное состояние по клику на основную иконку
+var onMainPinClick = document.querySelector('.map__pin--main');
+onMainPinClick.addEventListener('mouseup', function () {
+  activeModeOn();
+  fillAdress(570, 375);
+});
+
+//Заполнение карточки выбранной метки по клику
+
+mapPins.addEventListener('click', function (event) {
+  var target = event.target;
+  if (!(target.type === 'button')) {
+    target = target.parentElement;
+  }
+  for (var i = 0; i < mapPins.children.length; i++) {
+    if (mapPins.children[i] === target) {
+      var childIndex = i;
+    }
+  }
+  if (childIndex === 0 || childIndex === 1) {
+    var pinIndex = 0;
+  } else pinIndex = childIndex - 2;
+  fillMainCard(pinIndex);
+});
